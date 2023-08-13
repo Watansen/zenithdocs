c5c2406978686ac6f338528e9873f17ae650932d
Watansen <129324550+Watansen@users.noreply.github.com>
2023-08-14 02:15:48 +0700

    Update DocumentCard.tsx
    matching the props

--- nextjs-app/components/DocumentCard.tsx
+++ nextjs-app/components/DocumentCard.tsx
@@ -2,20 +2,23 @@ import React from 'react';
 import styles from '../styles/DocumentCard.module.css';
 
 interface DocumentCardProps {
-  title: string;
-  description: string;
-  author: string;
-  date: string;
+  document: {
+    title: string;
+    content: string;
+    accessLevel: string;
+  };
 }
 
-const DocumentCard: React.FC<DocumentCardProps> = ({ title, description, author, date }) => {
+const DocumentCard: React.FC<DocumentCardProps> = ({ document }) => {
+  const { title, content, accessLevel } = document;
+
   return (
     <div className={styles.card}>
       <h2 className={styles.title}>{title}</h2>
-      <p className={styles.description}>{description}</p>
+      <p className={styles.description}>{content}</p>
       <div className={styles.details}>
-        <span className={styles.author}>{author}</span>
-        <span className={styles.date}>{date}</span>
+        <span className={styles.accessLevel}>{accessLevel}</span>
+        {/* You might want to add more details here */}
       </div>
     </div>
   );
